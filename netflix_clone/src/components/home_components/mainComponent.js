6408ff26c9028460dcc4fe6b809091f99f7d5e6f
Manish <manishpaudel290@gmail.com>
2024-05-15 23:23:10 +0545

    main container file updated

--- netflix_clone/src/components/home_components/mainComponent.js
+++ netflix_clone/src/components/home_components/mainComponent.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import VideoScreen from './videoScreen';
 import VideoTitle from './videoTitle';
+import { useSelector } from 'react-redux';
 
 const MainComponent = () => {
+  const movie = useSelector((store)=>store.movie?.nowPlayingMovies);
+  if(!movie) return ;//early return if not found the movie
+
+  const {id,title,overview}= movie[1];
+  
   return (
     <div>
-        <VideoTitle/>
-      <VideoScreen/>
+        <VideoTitle title={title} description={overview}/>
+      <VideoScreen movieId={id}/>
     </div>
   );
 }
