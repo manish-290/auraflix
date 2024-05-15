6fe2da8fd52ed7c33d66707656b0b19d6065da21
Manish <manishpaudel290@gmail.com>
2024-05-15 23:25:45 +0545

    movie card updated

--- netflix_clone/src/components/home_components/movieCard.js
+++ netflix_clone/src/components/home_components/movieCard.js
@@ -5,9 +5,9 @@ const MovieCard = ({posterPath, title}) => {
   return (
     <div className='w-80 pr-2'>
       <img src={`${TMDB_IMG_URL}/${posterPath}`} alt="chamkila banner" />
-      <div>
+      {/* <div>
         <h1 className='text-white font-bold text-2xl'>{title}</h1>
-      </div>
+      </div> */}
     </div>
 
   );
